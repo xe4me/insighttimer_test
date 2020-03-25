5de1a938c208b6917d0a92af7a9fdc66b19e39fd
xe4me <smiladhi@gmail.com>
2020-03-25 19:49:20 +1100

    Showcasing Photo in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import Photo from './components/photo/Photo';
 
 function App() {
   return (
     <div className="App">
-     
+     <Photo src={'https://image.shutterstock.com/image-photo/white-transparent-leaf-on-mirror-260nw-577160911.jpg'}
+            id={'test'} caption={'Photo Title'} alt={'asdsad'}/>
     </div>
   );
 }
